7b1ee457ba0f141ee0f35af8a87456e022ec17c7
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-18 11:45:54 -0400

    Détection du numéro dans url /mouton/3/

--- service.js
+++ service.js
@@ -10,9 +10,10 @@ var repondre = async function(requete, reponse)
 			var listeMoutons = await moutonDAO.listerMoutons();
 			reponse.end(JSON.stringify(listeMoutons));			
 		}
-		if(requete.url.match(/\/mouton\/[0-9]+\/?/))
+		if(trouvailles = requete.url.match(/\/mouton\/([0-9]+)\/?/))
 		{
-			reponse.end('match un seul mouton');
+			numero = trouvailles[1];
+			reponse.end('match un seul mouton ' + numero);
 		}
 	}
 	if('POST' === requete.method)
