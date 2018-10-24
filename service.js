ef7fa944623ed0516e45bef2ab633e171a1aca77
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-24 14:07:47 -0400

    Decoupage de toutes les valeurs d'un formulaire

--- service.js
+++ service.js
@@ -16,13 +16,27 @@ var repondre = async function(requete, reponse)
 			console.log('match un seul mouton ' + numero);
 			
 			var mouton = await moutonDAO.chercherMouton(numero);
-			console.log('mouton recu par le service ' + JSON.stringify(mouton));
+			console.log('mouton recu de la base ' + JSON.stringify(mouton));
 			reponse.end(JSON.stringify(mouton));
 		}
 	}
 	if('POST' === requete.method)
 	{
-		
+		var data = ''; 
+		requete.on('data', function(message){ data += message;});
+		requete.on('end', function()
+		{
+			console.log(JSON.stringify(data));
+			var listeChamps = data.split("&");
+			for(var position in listeChamps)
+			{
+				var champs = listeChamps[position];
+				//console.log(champs);
+				[cle, valeur] = champs.split("=");
+				console.log(cle + " = " + valeur);
+				
+			}
+		});
 	}
 	reponse.end('');
 }
